078178a5b2e6b7fbcfd81f537f77c8a9cbe6901e
Sam <74021826+x39OME@users.noreply.github.com>
2023-09-02 10:30:10 +0300

    Delete 5 - React Basics directory

--- 5 - React Basics/Week 1 - React Components/lab/2-creating-and-importing-components/src/Heading.js
+++
@@ -1,7 +0,0 @@
-function Heading() {
-  return (
-    <h1>This is the Heading component.</h1>
-  )
-}
-
-export default Heading;
--- 5 - React Basics/Week 3 - Navigation, Updating and Assets in React.js/lab/1.creating-a-route/src/AboutLittleLemon.js
+++
@@ -1,7 +0,0 @@
-function AboutLittleLemon() {
-    return (
-        <h1>About Little Lemon</h1>
-    );
-}
-
-export default AboutLittleLemon;
